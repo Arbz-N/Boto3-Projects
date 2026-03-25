b8af85fa9995190ba213eafa8b8ce15c43f03e76
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-03-25 21:54:59 +0500

    updated consumer.py in Stream-Data-Processing-with-AmazonKinesis-using-boto3

--- Stream-Data-Processing-with-AmazonKinesis-using-boto3/consumer.py
+++ Stream-Data-Processing-with-AmazonKinesis-using-boto3/consumer.py
@@ -1,6 +1,3 @@
-# consumer.py
-# Kinesis stream se real-time orders receive karo
-
 import boto3
 import json
 import time
@@ -11,37 +8,24 @@ stream_name = 'Ecommerce-Orders-Stream'
 kinesis_client = boto3.client('kinesis', region_name=region_name)
 
 def consume_orders():
-    # Step 1: ShardIterator nikalo — reading position set karo
     shard_iterator = kinesis_client.get_shard_iterator(
         StreamName=stream_name,
-        # ShardId='shardId-000000000000',
         ShardId='shardId-000000000001',
-        # ↑ Pehle shard se shuru karo
-        # ↑ aws kinesis describe-stream se IDs dekho
         ShardIteratorType='LATEST'
-        # ↑ LATEST = abhi se nayi records padho
-        # ↑ Purani records ignore karo
     )['ShardIterator']
-    # ↑ ['ShardIterator'] = response dict se token nikalo
 
     print("Consumer started — waiting for orders...")
 
     while True:
-        # Step 2: Records padho
         records = kinesis_client.get_records(
             ShardIterator=shard_iterator,
             Limit=10
-            # ↑ Ek baar mein max 10 records
         )
 
-        # Step 3: Next position update karo
         shard_iterator = records['NextShardIterator']
-        # ↑ ZAROORI — agar update nahi kiya to same records dobara milenge ❌
 
-        # Step 4: Records process karo
         for record in records['Records']:
             order = json.loads(record['Data'])
-            # ↑ JSON string → Python dict
             print(f" {order.get('order_id','N/A')} | "
                   f"{order.get('product_name','N/A')[:28]:<28} | "
                   f"Qty:{order.get('quantity','N/A')} | "
@@ -50,6 +34,6 @@ def consume_orders():
                   f"{order.get('payment_method','N/A')}")
 
         time.sleep(2)
-        # ↑ 2 sec wait karo — polling delay
+        # ↑ 2 sec wait  — polling delay
 
 consume_orders()
